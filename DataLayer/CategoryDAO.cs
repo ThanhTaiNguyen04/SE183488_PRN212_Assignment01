7e51c23a4c816be36662300e8b2275d47bccfbfd
Tài Nguyễn <taintse183488@fpt.edu.vn>
2025-06-26 22:10:16 +0700

    Add files via upload

---
+++ DataLayer/CategoryDAO.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+
+namespace DataLayer
+{
+    public class CategoryDAO
+    {
+        
+        static List<Category> categories = new List<Category>();
+        private bool isGenerated = false;
+        public List<Category> GenerateSampleDataset()
+            {
+            if (isGenerated) {
+                return categories;
+            }
+
+                categories.Add(new Category()
+                {
+                    CategoryID = 1,
+                    CategoryName = "Thực phẩm đóng hộp",
+                    Description = "Cá hộp, thịt hộp, đậu đóng hộp, trái cây đóng hộp"
+                });
+
+                categories.Add(new Category()
+                {
+                    CategoryID = 2,
+                    CategoryName = "Gia dụng",
+                    Description = "Chất tẩy rửa, nước lau nhà, khăn giấy, túi rác"
+                });
+
+                categories.Add(new Category()
+                {
+                    CategoryID = 3,
+                    CategoryName = "Chăm sóc cá nhân",
+                    Description = "Dầu gội, sữa tắm, kem đánh răng, dao cạo"
+                });
+
+                categories.Add(new Category()
+                {
+                    CategoryID = 4,
+                    CategoryName = "Trái cây & Rau củ",
+                    Description = "Rau xanh, củ quả, trái cây tươi"
+                });
+
+                categories.Add(new Category()
+                {
+                    CategoryID = 5,
+                    CategoryName = "Đồ ăn nhanh",
+                    Description = "Mì ăn liền, snack, bánh tráng, khoai tây chiên"
+                });
+
+            isGenerated = true;
+                return categories;
+            }
+
+            public List<Category> GetCategories()
+            {
+                return categories;
+            }
+
+            public bool AddCategory(Category category)
+            {
+                Category c = categories.FirstOrDefault(c => c.CategoryID == category.CategoryID);
+                if (c != null)
+                {
+                    return false; 
+                }
+
+                categories.Add(category);
+                return true;
+            }
+
+            public bool RemoveCategory(int categoryId)
+            {
+                Category c = categories.FirstOrDefault(c => c.CategoryID == categoryId);
+                if (c == null)
+                {
+                    return false;
+                }
+
+                categories.Remove(c);
+                return true;
+            }
+
+            public Category SearchCategory(int categoryId)
+            {
+                return categories.FirstOrDefault(c => c.CategoryID == categoryId);
+            }
+
+            public bool UpdateCategory(Category category)
+            {
+                Category c = categories.FirstOrDefault(c => c.CategoryID == category.CategoryID);
+                if (c == null)
+                {
+                    return false;
+                }
+
+                c.CategoryName = category.CategoryName;
+                c.Description = category.Description;
+
+                return true;
+            }
+    }
+}
+
---
+++ DataLayer/CustomerDAO.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BusinessLayer;
+
+namespace DataLayer
+{
+    public class CustomerDAO
+    {
+        static List<Customer> customers = new List<Customer>();
+        private bool isGenerated = false;
+        public List<Customer> GenerateSampleDataset()
+        {
+            if(isGenerated) {
+                return customers;
+            }
+
+            customers.Add(new Customer()
+            {
+                CustomerID = 1,
+                CompanyName = "Công ty Thịnh Vượng",
+                ContactName = "Nguyễn Thành Tài",
+                ContactTitle = "Giám đốc điều hành",
+                Address = "101 Lý Thường Kiệt, Q.Tân Bình",
+                Phone = "0912345678"
+            });
+
+            customers.Add(new Customer()
+            {
+                CustomerID = 2,
+                CompanyName = "CTCP An Phát",
+                ContactName = "Nguyễn Thị Mai",
+                ContactTitle = "Phó phòng kinh doanh",
+                Address = "32 Nguyễn Văn Cừ, Q.5",
+                Phone = "0912987654"
+            });
+
+            customers.Add(new Customer()
+            {
+                CustomerID = 3,
+                CompanyName = "DNTN Hưng Thịnh",
+                ContactName = "Phạm Văn Tùng",
+                ContactTitle = "Nhân viên hỗ trợ",
+                Address = "66 Phan Đăng Lưu, Q.Bình Thạnh",
+                Phone = "0933123456"
+            });
+
+            customers.Add(new Customer()
+            {
+                CustomerID = 4,
+                CompanyName = "Công ty Ánh Dương",
+                ContactName = "Trần Thị Hồng",
+                ContactTitle = "Trưởng phòng nhân sự",
+                Address = "12 Trường Chinh, Q.Tân Phú",
+                Phone = "0966123456"
+            });
+
+            customers.Add(new Customer()
+            {
+                CustomerID = 5,
+                CompanyName = "CT TNHH Sao Mai",
+                ContactName = "Võ Minh Long",
+                ContactTitle = "Quản lý dự án",
+                Address = "88 Điện Biên Phủ, Q.3",
+                Phone = "0988112233"
+            });
+
+            isGenerated = true;
+            return customers;
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return customers;
+        }
+
+        public bool AddCustomer(Customer customer)
+        {
+            Customer c = customers.FirstOrDefault(c => c.CustomerID == customer.CustomerID);
+            if (c != null)
+            {
+                return false;
+            }
+
+            customers.Add(customer);
+            return true;
+        }
+
+        public bool RemoveCustomer(int customerId)
+        {
+            Customer c = customers.FirstOrDefault(c => c.CustomerID == customerId);
+            if (c == null)
+            {
+                return false;
+            }
+
+            customers.Remove(c);
+            return true;
+        }
+
+        public Customer SearchCustomer(int customerId)
+        {
+            return customers.FirstOrDefault(c => c.CustomerID == customerId);
+        }
+
+        public bool UpdateCustomer(Customer customer)
+        {
+            Customer c = customers.FirstOrDefault(c => c.CustomerID == customer.CustomerID);
+            if (c == null)
+            {
+                return false;
+            }
+
+            c.CompanyName = customer.CompanyName;
+            c.ContactName = customer.ContactName;
+            c.ContactTitle = customer.ContactTitle;
+            c.Address = customer.Address;
+            c.Phone = customer.Phone;
+
+            return true;
+        }
+    }
+}
